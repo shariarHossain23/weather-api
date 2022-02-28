0e5d2d1db842c9b60ffea0e67a154d3dfd932f0a
shariarhossain23e <shariarhossain23@gamil.comm>
2022-02-28 19:38:20 +0600

    some style and loading addr

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const searchWeather = () => {
     const searchInput = document.getElementById('search-input');
     const searchValue = searchInput.value.toLowerCase();
+    searchInput.value = "";
+    document.getElementById("spinner").style.display = "block"
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=dc6dadeda7ffd879b61c4971607ab8ce`)
         .then(res => res.json())
         .then(res => getWeather(res))
 }
 
 const getWeather = data => {
+
+    document.getElementById("spinner").style.display = "none"
     const cityName = data.name;
     const temp = data.main.temp;
     const cel  = temp -  273.15 ;
