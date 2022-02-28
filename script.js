510f5c17492704ef32bed81e9019c1396455519a
shariarhossain23e <shariarhossain23@gamil.comm>
2022-02-28 19:09:31 +0600

    weather

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-fetch('https:api.openweathermap.org/data/2.5/weather?lat=0.000000&lon=0.000000&appid=1f3c126c36b756e6cd712f00d03c25ae')
-.then(res => res.json())
-.then(data => console.log(data))
+const searchWeather = () => {
+    const searchInput = document.getElementById('search-input');
+    const searchValue = searchInput.value.toLowerCase();
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=dc6dadeda7ffd879b61c4971607ab8ce`)
+        .then(res => res.json())
+        .then(res => getWeather(res))
+}
+
+const getWeather = data => {
+    const cityName = data.name;
+    const temp = data.main.temp;
+    const cel  = temp -  273.15 ;
+    const desc = data.weather[0].description
+    document.getElementById("city").innerText = cityName;
+    document.getElementById("temperuture").innerText = Math.ceil(cel) + "°C";
+    document.getElementById("desc").innerText = desc
+}
